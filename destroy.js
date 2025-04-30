0dfda9a1fb67d30dc8f548ecdffe75f7092aa365
Jeff Horton <jeff@goblinoid.co.uk>
2025-04-30 23:01:16 +0100

    remove invalid return

--- destroy.js
+++ destroy.js
@@ -4,21 +4,22 @@ import {resolve} from 'node:path';
 
 try {
     if(core.getInput('destroy-stack-on-completion') === 'false') {
-        console.log("Action has been configured not to destroy the staging stack, skipping...")
-        return
-    }
+        core.warning(
+            "Action has been configured not to destroy the staging stack, skipping..."
+        );
+    } else {
+        const destroyAction = core.getInput('cdk-post-command');
+        const cwd = resolve(core.getInput('working-directory'));
 
-    const destroyAction = core.getInput('cdk-post-command');
-    const cwd = resolve(core.getInput('working-directory'));
+        const command = spawnSync(
+            'npm',
+            ['run', destroyAction],
+            {stdio: 'inherit', shell: true, cwd}
+        );
 
-    const command = spawnSync(
-        'npm',
-        ['run', destroyAction],
-        {stdio: 'inherit', shell: true, cwd}
-    );
-
-    if (command.status !== 0) {
-        core.setFailed(`Command failed with exit code ${command.status}`);
+        if (command.status !== 0) {
+            core.setFailed(`Command failed with exit code ${command.status}`);
+        }
     }
 } catch (error) {
     core.setFailed(error)
